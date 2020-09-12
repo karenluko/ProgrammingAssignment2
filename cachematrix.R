310c9ebd61775ad8da9b8be8db5fa2576585b602
karenluko <karen.luko@unesp.br>
2020-09-12 00:02:50 -0300

    Answer to Peer Reviewed Assignment: Lexical Scope

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { # set the function to require a matrix as input
+        m <- NULL #set object class as NULL
+        set <- function(y){ 
+                x <<- y #set a value y to the matrix
+                m <<- NULL
+        }
+        get <- function(){x} #get the value of the matrix
+        setInverse <- function(inverse) {m <<- inverse} #calculates the inverse of the matrix
+        getInverse <- function() {m} 
+        list(set = set, get = get, 
+             setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()#Return a matrix that is the inverse of 'x' and assign it to 'm'
+        if(!is.null(m)) # checks if the inverse has already been calculated
+                message("getting cached data") #if so, displays "getting cached data" message
+        return(m) #and retrieves it from cache memory
+        
+        matrix <- x$get()   #if the inverse has not been calculated, it will be done by the following function
+        m <- solve(matrix,...)
+        x$setInverse(m)
+        m
 }
